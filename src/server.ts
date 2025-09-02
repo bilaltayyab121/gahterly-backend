def2c58c6b2ed1c1cc2fdbbda5a038ad8912b5cc
bilaltayyab121 <bilaltayyab121@gmail.com>
2025-09-02 19:48:15 +0500

    test 6

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "https://gatherly-frontend-mu.vercel.app",
+      "http://localhost:3000",
+    ],
     credentials: true,
   })
 );
