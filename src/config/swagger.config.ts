25b02dc6a3edc731086fd7bcb5fcc2157292ce3b
bilaltayyab121 <bilaltayyab121@gmail.com>
2025-09-03 05:15:52 +0500

    test 7

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,6 +1,7 @@
 import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import { Express } from "express";
+
 const options: swaggerJSDoc.Options = {
   definition: {
     openapi: "3.0.0",
@@ -11,13 +12,15 @@ const options: swaggerJSDoc.Options = {
     },
     servers: [
       {
-        url: "http://localhost:5000/api",
+        url: "https://gatherly-backend.vercel.app/", // ✅ fixed typo
       },
     ],
   },
-  apis: ["./src/routes/*.ts"], // where Swagger looks for annotations
+  apis: ["./src/routes/*.ts", "./dist/routes/*.js"], // ✅ works in dev + prod
 };
+
 const swaggerSpec = swaggerJSDoc(options);
+
 export const setupSwagger = (app: Express) => {
   app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 };
